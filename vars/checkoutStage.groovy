1145133e3aee8d3f0d75c060f19db306a3502b60
ducmanh27 <phanmanhk57a1@gmail.com>
2025-09-06 10:27:06 +0700

    Add log branch and tag
    Signed-off-by: ducmanh27 <phanmanhk57a1@gmail.com>

--- vars/checkoutStage.groovy
+++ vars/checkoutStage.groovy
@@ -4,10 +4,17 @@ def call() {
         script {
             env.GIT_COMMIT_SHORT = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
             env.GIT_COMMIT_MSG   = sh(script: 'git log -1 --pretty=%B', returnStdout: true).trim()
-            env.BRANCH_NAME      = sh(script: 'git rev-parse --abbrev-ref HEAD', returnStdout: true).trim()
+            
+            // Lấy branch từ Jenkins nếu có, fallback rỗng
+            env.BRANCH_NAME = env.BRANCH_NAME ?: ''
+
+            // Lấy tag nếu commit có tag
+            env.GIT_TAG = sh(script: "git describe --tags --exact-match || echo ''", returnStdout: true).trim()
+
             echo "📋 Commit: ${env.GIT_COMMIT_SHORT}"
             echo "💬 Message: ${env.GIT_COMMIT_MSG}"
             echo "🌿 Branch: ${env.BRANCH_NAME}"
+            echo "🏷️ Tag: ${env.GIT_TAG}"
         }
     }
 }
